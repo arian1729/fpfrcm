874036308394ec8eb4752c5e2a57a6bfa2fc0b0a
arian1729 <you@example.com>
2024-03-03 12:20:53 -0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,42 @@
 <?php
-
+ 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HtmlController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
+ 
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+
+    
+
+
+});
+ 
+Route::group(['middleware' => 'auth'], function () {
+  
+   
+
+
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    
+    Route::get('/home', [HomeController::class, 'iindex']);
+    // Route::get('/invoice/create-invoice', [InvoiceController::class, 'invoice']);
+    // Route::post('/create-invoice', [InvoiceController::class, 'InvoicePost']);
+    
+  Route::get('/category/index',[CategoryController::class, 'index']);
+  Route::post('category',[CategoryController::class, 'IndexPost']);
+
+  
+  
+  
+});
+
